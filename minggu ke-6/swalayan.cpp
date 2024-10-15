2b2e0a67d3db9c73dfbeb6fa43c7415f8c6b3991
melinda-ab <melindaannastasiabudijono@mail.ugm.ac.id>
2024-10-15 09:33:40 +0000

    Updated project

---
+++ minggu ke-6/swalayan.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct barang{
+    int kode;
+    string nama;
+    int harga;
+    int jumlah;
+};
+
+int main(){
+    int n;
+    int totaljumlah=0;
+    int hargatotal=0;
+    int harga;
+    cout<<" Banyak Jenis Barang: ";
+    cin>>n;
+    cout<<endl;
+    if(n<=50){
+        cout<<" ----------------------------"<<endl<<endl;
+        cout<<" Data Jenis Barang"<<endl;
+        barang x[n];
+        int y[n];
+        for(int i=0;i<=(n-1);i++){
+            cout<<" Kode barang: ";
+            cin>>x[i].kode;
+            cout<<" Nama barang: ";
+            cin>>x[i].nama;
+            cout<<" Harga barang: ";
+            cin>>x[i].harga;
+            cout<<" Jumlah barang: ";
+            cin>>x[i].jumlah;
+            cout<<endl;
+        }
+        cout<<" ----------------------------"<<endl<<endl;
+        cout<<" Data Jumlah Setiap Barang"<<endl;
+        for(int i=0;i<=(n-1);i++){
+            cout<<" Nama barang: "<<x[i].nama<<endl;
+            cout<<" Jumlah barang: "<<x[i].jumlah<<endl;
+            totaljumlah += x[i].jumlah;
+            y[i]=x[i].harga * x[i].jumlah;
+            hargatotal += y[i];
+            cout<<endl;
+        }
+        cout<<" ----------------------------"<<endl<<endl;
+        cout<<" Total Jumlah Barang: "<<totaljumlah<<endl;
+        cout<<" Total Harga Semua Barang: "<<hargatotal<<endl;
+        harga = hargatotal/totaljumlah;
+        cout<<" Rata-rata Semua Barang: "<<harga<<endl;
+
+    } else {
+        cout<<" Kapasitas 50 Jenis Barang"<<endl;
+    }
+    return 0;
+}
