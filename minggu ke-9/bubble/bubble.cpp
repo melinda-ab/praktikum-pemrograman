3e2dc6171433e20473c310d102b61a9aab67df89
melinda-ab <melindaannastasiabudijono@mail.ugm.ac.id>
2024-11-10 22:40:13 +0000

    Updated project

---
+++ minggu ke-9/bubble/bubble.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+    string nisn;
+    string nama;
+    float values;
+};
+
+void bubblesortnisn(mahasiswa data[],int n){
+    mahasiswa t;
+    for(int i=0;i<=n-1;i++){
+        for (int j=0;j<=(n-2);j++){
+            if (data[j].nisn > data [j+1].nisn){
+                t=data[j];
+                data[j]=data[j+1];
+                data[j+1]=t;
+            }
+        }
+    }
+}
+
+void bubblesortvalues(mahasiswa data[],int n){
+    mahasiswa t;
+    for(int i=0;i<=n-1;i++){
+        for (int j=0;j<=(n-2);j++){
+            if (data[j].values > data [j+1].values ||
+             (data[j].values == data [j+1].values && data[j].nisn < data [j+1].nisn)){
+                t=data[j];
+                data[j]=data[j+1];
+                data[j+1]=t;
+            }
+        }
+    }
+}
+
+int main(){
+    mahasiswa data[] = {
+        {"9960312699", "Handi Ramadhan", 90},
+        {"9963959682", "Rio Alfandra", 55} ,
+        {"9950310962", "Ronaldo Valentino Uneputty", 80},
+        {"9970272750", "Achmad Yaumil Fadjri R.", 60},
+        {"9970293945", "Alivia Rahma Pramesti", 70},
+        {"9952382180", "Ari Lutfianto", 65},
+        {"9965653989", "Arief Budiman", 60}
+    };
+    int n = sizeof(data) / sizeof(data[0]);
+    
+    bubblesortnisn(data, n);
+    cout << "Data diurutkan berdasarkan NISN:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout<<"NISN:    "<<data[i].nisn<<endl;
+        cout<<"Nama:    "<<data[i].nama<<endl;
+        cout<<"Values:  "<<data[i].values<<endl;
+    }
+
+    return 0;
+}
---
+++ minggu ke-9/selection/selection.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+    string nisn;
+    string nama;
+    float values;
+};
+
+void selectionsortnisn(mahasiswa data[],int n){
+    for (int i = 0; i < (n-1); i++) {
+        int min=i;
+        for (int j = i+1; j < n; j++) {
+            if (data[j].nisn < data[min].nisn){
+                min=j;
+            }
+        }
+        mahasiswa temp = data[i];
+        data[i] = data[min];
+        data[min] = temp;
+    }
+}
+
+void selectionsortvalues(mahasiswa data[],int n){
+    for (int i = 0; i < (n-1); i++) {
+        int min=i;
+        for (int j = i+1; j < n; j++) {
+            if (data[j].values < data[min].values){
+                min=j;
+            }
+        }
+        mahasiswa temp = data[i];
+        data[i] = data[min];
+        data[min] = temp;
+    }
+}
+
+int main(){
+    mahasiswa data[] = {
+        {"9960312699", "Handi Ramadhan", 90},
+        {"9963959682", "Rio Alfandra", 55} ,
+        {"9950310962", "Ronaldo Valentino Uneputty", 80},
+        {"9970272750", "Achmad Yaumil Fadjri R.", 60},
+        {"9970293945", "Alivia Rahma Pramesti", 70},
+        {"9952382180", "Ari Lutfianto", 65},
+        {"9965653989", "Arief Budiman", 60}
+    };
+    int n = sizeof(data) / sizeof(data[0]);
+    
+    selectionsortnisn(data, n);
+    cout << "Data diurutkan berdasarkan NISN:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout<<"NISN:    "<<data[i].nisn<<endl;
+        cout<<"Nama:    "<<data[i].nama<<endl;
+        cout<<"Values:  "<<data[i].values<<endl;
+    }
+
+    cout<<endl;
+    selectionsortvalues(data, n);
+    cout << "Data diurutkan berdasarkan Values:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout<<"Values:  "<<data[i].values<<endl;
+        cout<<"Nama:    "<<data[i].nama<<endl;
+        cout<<"NISN:    "<<data[i].nisn<<endl;
+    }
+    return 0;
+}
