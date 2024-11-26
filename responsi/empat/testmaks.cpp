f19fb14df99efc932c68bd3600cf50b07ca33137
melinda-ab <melindaannastasiabudijono@mail.ugm.ac.id>
2024-11-26 07:37:52 +0000

    Updated Projects

--- responsi/empat/testmaks.cpp
+++ responsi/empat/testmaks.cpp
@@ -5,22 +5,50 @@ int main(){
     cout<<" Temukan Nilai Maksimum dalam Daftar"<<endl;
     cout<<" ==================================="<<endl;
     int x[5]={3,6,2,8,10};
-    int expected = 10;
-    cout<<" Nilai dalam daftar: ";
+    int y[3]={52,72,29};
+    int z[7]={2,87,21,19,32,65,94};
+    int expectedx = 10;
+    int expectedy = 72;
+    int expectedz = 94;
+    cout<<" Nilai dalam daftar x: ";
     for(int i=0; i<5; i++){
         cout<<x[i]<<" ";
     }
-    int max=x[0];
+    cout<<endl;
+    cout<<" Nilai dalam daftar y: ";
     for(int i=0; i<5; i++){
-        if(max<x[i]){
-            max=x[i];
+        cout<<y[i]<<" ";
+    }
+    cout<<endl;
+    cout<<" Nilai dalam daftar z: ";
+    for(int i=0; i<5; i++){
+        cout<<z[i]<<" ";
+    }
+    int maxx=x[0];
+    int maxy=y[0];
+    int maxz=z[0];
+    for(int i=0; i<5; i++){
+        if(maxx<x[i]){
+            maxx=x[i];
+        }
+    }
+    for(int i=0; i<3; i++){
+        if(maxy<y[i]){
+            maxy=y[i];
+        }
+    }
+    for(int i=0; i<7; i++){
+        if(maxz<z[i]){
+            maxz=z[i];
         }
     }
     cout<<endl;
-    cout<<" Nilai maksimum adalah "<<max<<endl;
+    cout<<" Nilai maksimum x adalah "<<maxx<<endl;
+    cout<<" Nilai maksimum y adalah "<<maxy<<endl;
+    cout<<" Nilai maksimum z adalah "<<maxz<<endl;
     cout<<" ==================================="<<endl;
     cout<<" Test mencari nilai maks: ";
-    if(max==expected){
+    if(maxx==expectedx && maxy==expectedy && maxz==expectedz){
         cout<<"Passed!"<<endl;
     } else{
         cout<<"Failed!"<<endl;
--- responsi/satu/testluas.cpp
+++ responsi/satu/testluas.cpp
@@ -3,19 +3,29 @@ using namespace std;
 
 int main(){
     float pi = 3.14;
-    int r=3;
-    float expected = 28.26;
+    int r1=3;
+    int r2=5;
+    int r3=10;
+    float expected1 = 28.26;
+    float expected2 = 78.5;
+    float expected3 = 314;
     cout<<"    Hitung Luas Lingkaran"<<endl;
     cout<<" ==========================="<<endl;
     cout<<" Rumus Lingkaran:"<<endl;
     cout<<" pi x jari-jari x jari-jari"<<endl;
     cout<<" ---------------------------"<<endl;
-    cout<<" Masukan nilai jari-jari: "<<r<<endl;
-    float a=pi*r*r;
-    cout<<" Luas lingkaran adalah "<<a<<endl;
+    cout<<" Masukan nilai jari-jari: "<<r1<<endl;
+    cout<<" Masukan nilai jari-jari: "<<r2<<endl;
+    cout<<" Masukan nilai jari-jari: "<<r3<<endl;
+    float a1=pi*r1*r1;
+    float a2=pi*r2*r2;
+    float a3=pi*r3*r3;
+    cout<<" Luas lingkaran adalah "<<a1<<endl;
+    cout<<" Luas lingkaran adalah "<<a2<<endl;
+    cout<<" Luas lingkaran adalah "<<a3<<endl;
     cout<<" ==========================="<<endl;
     cout<<" Test hitung luas: ";
-    if(a==expected){
+    if(a1==expected1 && a2==expected2 && a3==expected3){
         cout<<"Passed!"<<endl;
     } else{
         cout<<"Failed!"<<endl;
